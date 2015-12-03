4870651f55063785af34001b0ca3d4575c1b0223
xalbrech <d.albrecht@seznam.cz>
2015-12-03 22:20:37 +0100

    -server added as jvm parameter

--- truffle_benchmarks/src/xalbrech/expressions/simple/SimpleExpressionTests.java
+++ truffle_benchmarks/src/xalbrech/expressions/simple/SimpleExpressionTests.java
@@ -1,18 +1,26 @@
-package expressions.simple;
+package xalbrech.expressions.simple;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
+import xalbrech.expressions.simple.AddExpression;
+import xalbrech.expressions.simple.EvaluatedExpression;
+import xalbrech.expressions.simple.MulExpression;
+import xalbrech.expressions.simple.SimpleEvaluator;
+import xalbrech.expressions.simple.Variable;
+
 @BenchmarkMode(Mode.SampleTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
+@Fork(jvmArgs="-server")
 public class SimpleExpressionTests {
 	
 	private SimpleEvaluator evaluator;
--- truffle_benchmarks/src/xalbrech/expressions/truffle/TruffleExpressionTest.java
+++ truffle_benchmarks/src/xalbrech/expressions/truffle/TruffleExpressionTest.java
@@ -1,24 +1,32 @@
-package expressions.truffle;
+package xalbrech.expressions.truffle;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
+import xalbrech.expressions.truffle.AddExpression;
+import xalbrech.expressions.truffle.EvaluatedExpression;
+import xalbrech.expressions.truffle.MulExpression;
+import xalbrech.expressions.truffle.TruffleEvaluator;
+import xalbrech.expressions.truffle.Variable;
+
 @BenchmarkMode(Mode.SampleTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
+@Fork(jvmArgs="-server")
 public class TruffleExpressionTest {
 
 	private EvaluatedExpression expression;
 	private TruffleEvaluator evaluator;
 
-	@Setup
+	@Setup 
 	public void setUp() {
 		expression = new AddExpression(new Variable("A"), 
 				new MulExpression(new Variable("B"), 
