00058364b2aa962e1d04e9f53535a1ac4cfd9a62
xalbrech <d.albrecht@seznam.cz>
2015-12-29 20:39:58 +0100

    naive attempt to benchmark

--- jmh_demo/src/main/java/xalbrech/MyBenchmark.java
+++ jmh_demo/src/main/java/xalbrech/MyBenchmark.java
@@ -40,5 +40,7 @@ public class MyBenchmark {
         // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
         // Put your benchmark code here.
     }
+    
+    
 
 }
--- jmh_demo/src/main/java/xalbrech/SimpleStringTest.java
+++ jmh_demo/src/main/java/xalbrech/SimpleStringTest.java
@@ -1,5 +1,21 @@
 package xalbrech;
 
+
 public class SimpleStringTest {
+	
+	public static void main(String[] args) {
+		
+
+		 long start = System.currentTimeMillis();
+			String res = null;		 
+		for (int i = 0; i < 22_000; i++) {
+			res = ""; 
+			res = res + new String("aaa") + new String("bbb");
+		} 
+		
+		System.out.println(System.currentTimeMillis() - start);
+		
+		new StringBuilder(res);
+	}
 
 }
