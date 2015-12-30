334c9c844fe222fe28560533c3901acca99a5304
david <d.albrecht@seznam.cz>
2015-12-30 09:06:10 +0100

    Averaging several attempts

--- jmh_demo/src/main/java/xalbrech/SimpleStringTest.java
+++ jmh_demo/src/main/java/xalbrech/SimpleStringTest.java
@@ -1,21 +1,24 @@
 package xalbrech;
 
-
 public class SimpleStringTest {
-	
-	public static void main(String[] args) {
-		
 
-		 long start = System.currentTimeMillis();
-			String res = null;		 
-		for (int i = 0; i < 22_000; i++) {
-			res = ""; 
-			res = res + new String("aaa") + new String("bbb");
-		} 
-		
-		System.out.println(System.currentTimeMillis() - start);
-		
-		new StringBuilder(res);
+	public static String result;
+
+	public static void main(String[] args) {
+
+		long acc = 0;
+		for (int j = 0; j < 100; j++) {
+			long start = System.currentTimeMillis();
+			for (int i = 0; i < 3_000_000; i++) {
+				result = "";
+				result = "" + new String("aaa");
+			}
+			long timing = System.currentTimeMillis() - start;
+			System.out.println(timing);
+			acc += timing;
+		}
+
+		System.out.printf("Average: %d", acc / 100);
 	}
 
 }
