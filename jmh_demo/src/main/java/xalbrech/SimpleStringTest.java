ac3754d09dfaabf3c1a8565b5009a090a611577c
dalbrecht <dalbrecht@CZPADDALBRECHT3.infor.com>
2016-01-03 16:45:27 +0100

    variable delay (an extra loop before the actual measurement) added. This is to demonstrate the accuracy issues.

--- jmh_demo/src/main/java/xalbrech/SimpleStringTest.java
+++ jmh_demo/src/main/java/xalbrech/SimpleStringTest.java
@@ -8,23 +8,23 @@ public class SimpleStringTest {
 
 		for (int k = 0; k < 2000; k++) {
 
-			for (int i = 0; i < 10 * k; i++) {
-				result = "" + new String("aaa").replaceAll("a", "b");
-			}
-
 			long acc = 0;
 			for (int j = 0; j < 200; j++) {
-				long start = System.nanoTime();
-				for (int i = 0; i < 500; i++) {
-					result = "";
+				
+				for (int i = 0; i < 200 * (k % 40); i++) {
 					result = "" + new String("aaa").replaceAll("a", "b");
-				}
+				}				
+				
+				long start = System.nanoTime();
+				//for (int i = 0; i < 100; i++) {
+					result = "" + new String("aaa").replaceAll("a", "b");
+				//}
 				long timing = System.nanoTime() - start;
 				// System.out.println(timing);
 				acc += timing;
 			}
 
-			System.out.printf("%d\n", acc);
+			System.out.printf("%d\n", acc / 200);
 		}
 	}
 
